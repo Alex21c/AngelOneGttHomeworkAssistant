dafa553acc7515621a936377140ae2a5ef382991
Abhishek kumar <aasalex21c@gmail.com>
2024-06-14 17:31:59 +0530

    added new feature to automatically process acc to portfolio close 5%, now work is reamining for the when storing into json file, i need my avg buying price as well in excel

---
+++ config.mjs
@@ -0,0 +1,90 @@
+export const scriptsRealNames = {
+  BankBees: "BANKBEES-EQ",
+  GoldBees: "GOLDBEES-EQ",
+  JuniorBees: "JUNIORBEES-EQ",
+  Mid150Bees: "MID150BEES-EQ",
+  NiftyBees: "NIFTYBEES-EQ",
+  SilverBees: "SILVERBEES-EQ",
+};
+export const scriptsFormalNames = {
+  "BANKBEES-EQ": "BankBees",
+  "GOLDBEES-EQ": "GoldBees",
+  "JUNIORBEES-EQ": "JuniorBees",
+  "MID150BEES-EQ": "Mid150Bees",
+  "NIFTYBEES-EQ": "NiftyBees",
+  "SILVERBEES-EQ": "SilverBees",
+};
+
+export const investMentCapitalAllowedOneTenth = {
+  BankBees: 2000,
+  JuniorBees: 2000,
+  Mid150Bees: 2000,
+  NiftyBees: 2000,
+  GoldBees: 2000,
+  SilverBees: 2000,
+};
+
+export const tokensMap = new Map([
+  ["BankBees", "11439"],
+  ["JuniorBees", "10939"],
+  ["Mid150Bees", "8506"],
+  ["NiftyBees", "10576"],
+  ["GoldBees", "14428"],
+  ["SilverBees", "8080"],
+]);
+
+export const percentMappings = new Map([
+  ["_2_Percent", 2],
+  ["_5_Percent", 5],
+  ["_8_Percent", 8],
+  ["_3_Percent", 3],
+  ["_6_Percent", 6],
+  ["_9_Percent", 9],
+]);
+
+export const GttIds = {
+  yesterdayClose: {
+    _2_Percent: {
+      BankBees: 3392320,
+      JuniorBees: null,
+      Mid150Bees: null,
+      NiftyBees: 3392353,
+    },
+    _5_Percent: {
+      BankBees: 3392316,
+      JuniorBees: null,
+      Mid150Bees: null,
+      NiftyBees: 3369479,
+    },
+    _8_Percent: {
+      BankBees: 3369404,
+      JuniorBees: null,
+      Mid150Bees: null,
+      NiftyBees: 3369472,
+    },
+
+    // GOLD & SILVERBEES
+    _3_Percent: {
+      GoldBees: 2164053,
+      SilverBees: 3456208,
+    },
+    _6_Percent: {
+      GoldBees: 3369422,
+      SilverBees: 2626641,
+    },
+    _9_Percent: {
+      GoldBees: 2626646,
+      SilverBees: 3369493,
+    },
+  },
+  portfolio: {
+    _5_Percent: {
+      BankBees: 3369390,
+      GoldBees: null,
+      JuniorBees: null,
+      Mid150Bees: null,
+      NiftyBees: null,
+      SilverBees: null,
+    },
+  },
+};
--- index.mjs
+++ index.mjs
@@ -4,7 +4,14 @@ import { authenticator } from "otplib";
 import { SmartAPI, WebSocket, WebSocketV2 } from "smartapi-javascript";
 import fs from "node:fs";
 import chalk from "chalk";
-
+import {
+  GttIds,
+  percentMappings,
+  tokensMap,
+  investMentCapitalAllowedOneTenth,
+  scriptsFormalNames,
+  scriptsRealNames,
+} from "./config.mjs";
 const ChalkSuccess = chalk.green;
 const ChalkError = chalk.red;
 const ChalkWarning = chalk.yellow;
@@ -13,96 +20,35 @@ let smart_api = new SmartAPI({
   api_key: process.env.apiKey, // PROVIDE YOUR API KEY HERE
 });
 
-const scriptsRealNames = {
-  BankBees: "BANKBEES-EQ",
-  GoldBees: "GOLDBEES-EQ",
-  JuniorBees: "JUNIORBEES-EQ",
-  Mid150Bees: "MID150BEES-EQ",
-  NiftyBees: "NIFTYBEES-EQ",
-  SilverBees: "SILVERBEES-EQ",
-};
-const scriptsFormalNames = {
-  "BANKBEES-EQ": "BankBees",
-  "GOLDBEES-EQ": "GoldBees",
-  "JUNIORBEES-EQ": "JuniorBees",
-  "MID150BEES-EQ": "Mid150Bees",
-  "NIFTYBEES-EQ": "NiftyBees",
-  "SILVERBEES-EQ": "SilverBees",
-};
+function computeQuantityPortfolioSpecific(
+  investMentCapitalAllowedOneTenth,
+  scriptName,
+  limitPrice,
+  portfolioPercent,
+  holdingsPAndL
+) {
+  if (!holdingsPAndL[scriptName]) {
+    throw new Error(`failed to get holdingPAndL for ${scriptName}`);
+  }
 
-const investMentCapitalAllowedOneTenth = {
-  BankBees: 2000,
-  JuniorBees: 2000,
-  Mid150Bees: 2000,
-  NiftyBees: 2000,
-  GoldBees: 2000,
-  SilverBees: 2000,
-};
+  let quantity = investMentCapitalAllowedOneTenth[scriptName] / limitPrice;
+  if (portfolioPercent === 0.05) {
+    quantity *= 2;
+  }
 
-const tokensMap = new Map([
-  ["BankBees", "11439"],
-  ["JuniorBees", "10939"],
-  ["Mid150Bees", "8506"],
-  ["NiftyBees", "10576"],
-  ["GoldBees", "14428"],
-  ["SilverBees", "8080"],
-]);
+  // safeguard
+  if (
+    Number(holdingsPAndL[scriptName]) <=
+    Number(
+      process.env.setQuantityToOneForPortfolioIfClosingIsLessThanEqualToXPercent
+    )
+  ) {
+    return 1;
+  }
 
-const percentMappings = new Map([
-  ["_2_Percent", 2],
-  ["_5_Percent", 5],
-  ["_8_Percent", 8],
-  ["_3_Percent", 3],
-  ["_6_Percent", 6],
-  ["_9_Percent", 9],
-]);
+  return Math.ceil(quantity);
+}
 
-const GttIds = {
-  yesterdayClose: {
-    _2_Percent: {
-      BankBees: 3392320,
-      JuniorBees: null,
-      Mid150Bees: null,
-      NiftyBees: 3392353,
-    },
-    _5_Percent: {
-      BankBees: 3392316,
-      JuniorBees: null,
-      Mid150Bees: null,
-      NiftyBees: 3369479,
-    },
-    _8_Percent: {
-      BankBees: 3369404,
-      JuniorBees: null,
-      Mid150Bees: null,
-      NiftyBees: 3369472,
-    },
-
-    // GOLD & SILVERBEES
-    _3_Percent: {
-      GoldBees: 2164053,
-      SilverBees: 3456208,
-    },
-    _6_Percent: {
-      GoldBees: 3369422,
-      SilverBees: 2626641,
-    },
-    _9_Percent: {
-      GoldBees: 2626646,
-      SilverBees: 3369493,
-    },
-  },
-  portfolio: {
-    _5_Percent: {
-      BankBees: null,
-      GoldBees: null,
-      JuniorBees: null,
-      Mid150Bees: null,
-      NiftyBees: null,
-      SilverBees: null,
-    },
-  },
-};
 function computeQuantity(
   investMentCapitalAllowedOneTenth,
   scriptName,
@@ -133,6 +79,13 @@ function computeQuantity(
   return Math.ceil(quantity);
 }
 
+function getMyBuyingAveragePrice(
+  script,
+  mapMyBuyingAveragePrice,
+  scriptsRealNames
+) {
+  return mapMyBuyingAveragePrice.get(script);
+}
 function getClosingPrice(script, mapClosingPrices, scriptsRealNames) {
   return mapClosingPrices.get(scriptsRealNames[script]);
 }
@@ -150,6 +103,7 @@ async function modifyGTTAccToYesterdayClose(
   fetchedGtts
 ) {
   return new Promise((resolve, reject) => {
+    console.log(":: Modifying GTT according to Yesterday Close");
     let timeout = 1000;
     let promises = [];
     Object.entries(GttIds.yesterdayClose).forEach(([howMuchPercent, obj]) => {
@@ -166,6 +120,55 @@ async function modifyGTTAccToYesterdayClose(
                 percentMappings.get(howMuchPercent),
                 mapClosingPrices,
                 fetchedGtts
+              );
+              resolve();
+            }, timeout);
+          });
+
+          promises.push(promise);
+          timeout += Number(process.env.timeOutDelayBetweenModifyGttReq);
+          // now i want to get the yesterday closing price of current script
+        }
+      });
+      // console.log(obj)
+    });
+    Promise.all(promises)
+      .then(() => {
+        resolve("processing completed!");
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    // resolve("processing completed!");
+  });
+}
+
+async function modifyGTTAccToPortfolio5Percent(
+  GttIds,
+  mapMyBuyingAveragePrice,
+  fetchedGtts,
+  holdingsPAndL
+) {
+  return new Promise((resolve, reject) => {
+    console.log(mapMyBuyingAveragePrice);
+    console.log(":: Modifying GTT according to Portfolio");
+    let timeout = 1000;
+    let promises = [];
+    Object.entries(GttIds.portfolio).forEach(([howMuchPercent, obj]) => {
+      Object.entries(obj).forEach(([script, gttID]) => {
+        if (gttID) {
+          let promise = new Promise((resolve) => {
+            setTimeout(() => {
+              console.log(`\n:: Processing ${howMuchPercent}`);
+              console.log(`:: ${script}: ${gttID}`);
+              console.log(":: API req made to modify gtt!");
+              modifyGttAccToPortfolio(
+                gttID,
+                script,
+                percentMappings.get(howMuchPercent),
+                mapMyBuyingAveragePrice,
+                fetchedGtts,
+                holdingsPAndL
               );
               resolve();
             }, timeout);
@@ -245,20 +248,21 @@ async function fetchActiveGTTs(smart_api) {
 
 async function storeFetchedGttsIntoJsonFile(fetchedGtts) {
   try {
-    console.log("\n:: Saving fetched GTTs into JSON file!");
     if (process.env.isFetchedGttsStorageIntoJsonFileRequired === "true") {
+      console.log("\n:: Saving fetched GTTs into JSON file!");
       // console.log(response.data);
       const fetchedGttsObj = Object.fromEntries(fetchedGtts);
       fs.writeFileSync(
         process.env.jsonFileForStoringActiveFetchedGTTs,
         JSON.stringify(fetchedGttsObj, null, "\t")
       );
+
+      console.log(
+        ChalkSuccess(
+          `fetched GTTs stored successfully into ${process.env.jsonFileForStoringActiveFetchedGTTs}\n`
+        )
+      );
     }
-    console.log(
-      ChalkSuccess(
-        `fetched GTTs stored successfully into ${process.env.jsonFileForStoringActiveFetchedGTTs}\n`
-      )
-    );
   } catch (error) {
     throw error;
   }
@@ -334,15 +338,42 @@ async function saveIntoExcelFile(mapClosingPrices, holdingsPAndL) {
   }
 }
 
-async function fetchOverallGainLoss(smart_api) {
+async function fetchHoldings(smart_api) {
   try {
-    console.log(":: Fetching overall G/L");
+    console.log(":: Fetching Holdings");
     let holdings = await smart_api.getHolding();
     if (holdings) {
       holdings = holdings.data;
     } else {
-      throw new Error("failed to fetch overall P&L");
+      throw new Error("failed to fetch Holdings");
     }
+    return holdings;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function fetchMyBuyingAvergePrice(holdings) {
+  try {
+    console.log(":: Extracting My Buying Average Price from Holdings");
+    // creating simple mapping out of it
+    const mappingMyBuyingAveragePrice = new Map();
+    holdings.forEach((script) => {
+      const scriptFormalName = scriptsFormalNames[script.tradingsymbol];
+      if (scriptFormalName !== undefined) {
+        mappingMyBuyingAveragePrice.set(scriptFormalName, script.averageprice);
+      }
+    });
+
+    return mappingMyBuyingAveragePrice;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function fetchOverallGainLoss(holdings) {
+  try {
+    console.log(":: Extracting overall Gain/Loss from Holdings");
     // creating simple mapping out of it
     const mappingPAndL = {};
     holdings.forEach((script) => {
@@ -354,6 +385,7 @@ async function fetchOverallGainLoss(smart_api) {
 
     return mappingPAndL;
   } catch (error) {
+    console.log(error);
     throw error;
   }
 }
@@ -372,7 +404,7 @@ function modifyGtt(
       scriptsRealNames
     );
 
-    // 2% from yesterday close
+    // x% from yesterday close
     const xPercent = percentFromPreviousClose / 100;
     const limitPrice = Number(
       (closingPrice - closingPrice * xPercent).toFixed(2)
@@ -435,6 +467,87 @@ function modifyGtt(
   }
 }
 
+function modifyGttAccToPortfolio(
+  gttID,
+  scriptName,
+  percentFromPortfolio,
+  mapMyBuyingAveragePrice,
+  fetchedGtts,
+  holdingsPAndL
+) {
+  try {
+    const myBuyingAveragePrice = getMyBuyingAveragePrice(
+      scriptName,
+      mapMyBuyingAveragePrice,
+      scriptsRealNames
+    );
+
+    // x% from portfolio close
+    const xPercent = percentFromPortfolio / 100;
+    const limitPrice = Number(
+      (myBuyingAveragePrice - myBuyingAveragePrice * xPercent).toFixed(2)
+    );
+
+    const triggerPrice = roundOffTriggerPrice(limitPrice + 0.01);
+    // now i want to make sure that trigger price be either .05 or .00 whole number
+
+    const quantity = computeQuantityPortfolioSpecific(
+      investMentCapitalAllowedOneTenth,
+      scriptName,
+      limitPrice,
+      xPercent,
+      holdingsPAndL
+    );
+
+    console.log(
+      `script: ${scriptName}, myBuyingAveragePrice: ${myBuyingAveragePrice}, limitPrice: ${limitPrice}, triggerPrice:${triggerPrice}, quantity: ${quantity}`
+    );
+
+    const orderDetail = {
+      id: gttID,
+      symboltoken: tokensMap.get(scriptName),
+      exchange: "NSE",
+      qty: quantity,
+      price: limitPrice,
+      triggerprice: triggerPrice,
+    };
+    console.log(orderDetail);
+    // making API Call
+    // first let me check is modification actually required?
+
+    let isGttModificationRequired = true;
+    try {
+      let fetchedGtt = fetchedGtts.get(gttID);
+      // console.log(fetchedGtt);
+      if (
+        fetchedGtt.qty === orderDetail.qty &&
+        fetchedGtt.triggerprice === orderDetail.triggerprice &&
+        fetchedGtt.price === orderDetail.price
+      ) {
+        isGttModificationRequired = false;
+      }
+    } catch (error) {
+      console.log(ChalkError(error.message));
+      throw error;
+    }
+
+    if (isGttModificationRequired) {
+      if (process.env.isAllowedToModifyGTTOrders === "true") {
+        console.log(".env file allowed to modify GTT!");
+
+        modifyGttOrder(orderDetail, smart_api);
+      } else {
+        console.log(ChalkWarning("Not allowed to modify GTT, check .env!"));
+      }
+    } else {
+      console.log(ChalkSuccess("GTT modification not required! (skipping)"));
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to modify GTT, ", error.message);
+  }
+}
+
 async function init() {
   smart_api.setSessionExpiryHook(() => {
     console.log(":: +++++++++++++Logging Out");
@@ -456,7 +569,13 @@ async function init() {
     storeFetchedGttsIntoJsonFile(fetchedGtts);
 
     // fetching holding
-    const holdingsPAndL = await fetchOverallGainLoss(smart_api);
+    const holdings = await fetchHoldings(smart_api);
+
+    const holdingsPAndL = fetchOverallGainLoss(holdings);
+    // console.log(holdingsPAndL);
+
+    // fetch my buying average price
+    const mapMyBuyingAveragePrice = fetchMyBuyingAvergePrice(holdings);
 
     // Getting closing prices
     console.log(":: Fetching Closing Prices");
@@ -470,12 +589,18 @@ async function init() {
 
     // let me try to modify any gtt order
     console.log(":: Modifying GTT");
-    // i want its price to be 2% less than closing price
-
     await modifyGTTAccToYesterdayClose(GttIds, mapClosingPrices, fetchedGtts);
+
+    await modifyGTTAccToPortfolio5Percent(
+      GttIds,
+      mapMyBuyingAveragePrice,
+      fetchedGtts,
+      holdingsPAndL
+    );
 
     console.log("\n:: All done Gracefully !");
   } catch (error) {
+    console.log(error);
     console.log(ChalkError("ERROR: ", error.message));
   } finally {
     console.log("logged out!");
